de02db3e16f6959d2104f87b66caae41e164d6b7
sanya-shch <shchipkov.sanya@ukr.net>
2019-12-19 00:37:45 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/home/Homepage';
 
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE </h1>
+    </div>
+);
+
+const JacketsPage = () => (
+    <div>
+        <h1>JACKETS PAGE </h1>
+    </div>
+);
+
+const SneakersPage = () => (
+    <div>
+        <h1>SNEAKERS PAGE </h1>
+    </div>
+);
+
+const WomensPage = () => (
+    <div>
+        <h1>WOMENS PAGE </h1>
+    </div>
+);
+
+const MensPage = () => (
+    <div>
+        <h1>MENS PAGE </h1>
+    </div>
+);
+
 function App() {
   return (
       <div>
-        <Homepage />
+          <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route path='/hats' component={HatsPage} />
+              <Route path='/jackets' component={JacketsPage} />
+              <Route path='/sneakers' component={SneakersPage} />
+              <Route path='/womens' component={WomensPage} />
+              <Route path='/mens' component={MensPage} />
+          </Switch>
       </div>
   );
 }
--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -8,29 +8,34 @@ class Directory extends React.Component {
             {
                 title: 'hats',
                 imageUrl: 'https://images.unsplash.com/photo-1529958030586-3aae4ca485ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-                id: 1
+                id: 1,
+                linkUrl: 'hats'
             },
             {
                 title: 'jackets',
                 imageUrl: 'https://images.unsplash.com/photo-1561365452-adb940139ffa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1038&q=80',
-                id: 2
+                id: 2,
+                linkUrl: 'jackets'
             },
             {
                 title: 'sneakers',
                 imageUrl: 'https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-                id: 3
+                id: 3,
+                linkUrl: 'sneakers'
             },
             {
                 title: 'womens',
                 imageUrl: 'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: 'womens'
             },
             {
                 title: 'mens',
                 imageUrl: 'https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: 'mens'
             }
         ]
     };
@@ -39,8 +44,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ title, imageUrl, id, size }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
