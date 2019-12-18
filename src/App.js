8af4da772e4b2276c4a446c131f999e064991bcb
sanya-shch <shchipkov.sanya@ukr.net>
2019-12-18 23:53:07 +0200

    added menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Homepage from './pages/home/Homepage';
+
 function App() {
   return (
-    <div>
-
-    </div>
+      <div>
+        <Homepage />
+      </div>
   );
 }
 
