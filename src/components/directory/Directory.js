6083f10e88a69a134c26b4da703470e0fcc60f81
sanya-shch <shchipkov.sanya@ukr.net>
2019-12-18 23:56:32 +0200

    fix Directory.js

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -3,48 +3,46 @@ import MenuItem from '../menu-item/MenuItem';
 import './directory.styles.scss';
 
 class Directory extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://images.unsplash.com/photo-1529958030586-3aae4ca485ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-                    id: 1
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://images.unsplash.com/photo-1561365452-adb940139ffa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1038&q=80',
-                    id: 2
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-                    id: 3
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-                    size: 'large',
-                    id: 4
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-                    size: 'large',
-                    id: 5
-                }
-            ]
-        };
-    }
+    state = {
+        sections: [
+            {
+                title: 'hats',
+                imageUrl: 'https://images.unsplash.com/photo-1529958030586-3aae4ca485ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+                id: 1
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://images.unsplash.com/photo-1561365452-adb940139ffa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1038&q=80',
+                id: 2
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+                id: 3
+            },
+            {
+                title: 'womens',
+                imageUrl: 'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+                size: 'large',
+                id: 4
+            },
+            {
+                title: 'mens',
+                imageUrl: 'https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+                size: 'large',
+                id: 5
+            }
+        ]
+    };
 
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-                ))}
+                {
+                    this.state.sections.map(({ title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))
+                }
             </div>
         );
     }
